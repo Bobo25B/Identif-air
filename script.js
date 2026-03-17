76af8c0a6811e6042003e57b2386a0b4ddb5d151
Bobo25B <jualbonico@gmail.com>
2026-03-17 14:33:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,41 +8,43 @@ async function loadData() {
   showQuestion();
 }
 
-function nextQuestion() {
-
-  current++;
-
-  if (current >= data.length) {
-    shuffle(data);
-    current = 0;
+// Mélange du tableau
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-
-  showQuestion();
 }
+
+// Affiche la question courante
+function showQuestion() {
   let q = data[current];
 
   document.getElementById("planeImage").src = q.image;
   document.getElementById("result").innerText = "";
 }
 
+// Affiche la réponse
 function showAnswer() {
   let q = data[current];
   document.getElementById("result").innerText = "Réponse : " + q.answer;
 }
 
+// Passe à la question suivante en boucle et ordre aléatoire
 function nextQuestion() {
   current++;
 
   if (current >= data.length) {
-    alert("Toutes les images ont été vues !");
-    current = 0;
     shuffle(data);
+    current = 0;
   }
 
   showQuestion();
 }
 
+// Liens avec les boutons
 document.getElementById("answerBtn").onclick = showAnswer;
 document.getElementById("nextBtn").onclick = nextQuestion;
 
+// Chargement des données
 loadData();
